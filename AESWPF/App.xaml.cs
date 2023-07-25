030cce5ef4f42fbfb922c53c8597a2c265aea5b2
witmus <witold.muszynski123@wp.pl>
2023-07-25 22:16:34 +0200

    net7 migration, added exception message box

--- AESWPF/App.xaml.cs
+++ AESWPF/App.xaml.cs
@@ -13,5 +13,21 @@ namespace AESWPF
     /// </summary>
     public partial class App : Application
     {
+        private void StartupApplication(object sender, StartupEventArgs e)
+        {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(HandleDomainException);
+        }
+
+        private void HandleException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+
+            MessageBox.Show(e.Exception.Message);
+        }
+
+        private void HandleDomainException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show((e.ExceptionObject as Exception)?.Message ?? "exception null");
+        }
     }
 }
